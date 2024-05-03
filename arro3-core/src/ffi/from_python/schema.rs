0d5e672925000520a32464f7dcd4a37103b4ea43
Kyle Barron <kylebarron2@gmail.com>
2024-05-02 22:50:08 -0400

    Implement Field FFI (#5)

--- arro3-core/src/ffi/from_python/schema.rs
+++ arro3-core/src/ffi/from_python/schema.rs
@@ -1,11 +1,17 @@
+use std::sync::Arc;
+
 use crate::ffi::from_python::utils::import_arrow_c_schema;
 use crate::schema::PySchema;
+use arrow_schema::Schema;
+use pyo3::exceptions::PyTypeError;
 use pyo3::prelude::*;
 use pyo3::{PyAny, PyResult};
 
 impl<'a> FromPyObject<'a> for PySchema {
     fn extract(ob: &'a PyAny) -> PyResult<Self> {
-        let schema = import_arrow_c_schema(ob)?;
-        Ok(Self::new(schema))
+        let schema_ptr = import_arrow_c_schema(ob)?;
+        let schema =
+            Schema::try_from(schema_ptr).map_err(|err| PyTypeError::new_err(err.to_string()))?;
+        Ok(Self::new(Arc::new(schema)))
     }
 }
