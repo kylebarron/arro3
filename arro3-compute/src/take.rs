e5473d72988d2008f5cdf17e0ed4ae21187b04e8
Kyle Barron <kylebarron2@gmail.com>
2024-06-27 20:16:09 -0400

    Update pyo3-arrow readme

--- arro3-compute/src/take.rs
+++ arro3-compute/src/take.rs
@@ -3,9 +3,10 @@ use pyo3_arrow::error::PyArrowResult;
 use pyo3_arrow::PyArray;
 
 #[pyfunction]
-pub fn take(values: PyArray, indices: PyArray) -> PyArrowResult<PyObject> {
-    let (values_arr, values_field) = values.into_inner();
+pub fn take(py: Python, values: PyArray, indices: PyArray) -> PyArrowResult<PyObject> {
+    let (values_array, values_field) = values.into_inner();
     let (indices, _) = indices.into_inner();
-    let result = arrow_select::take::take(&values_arr, &indices, None)?;
-    Python::with_gil(|py| PyArray::new(result, values_field).to_arro3(py))
+    let output_array =
+        py.allow_threads(|| arrow_select::take::take(&values_array, &indices, None))?;
+    PyArray::new(output_array, values_field).to_arro3(py)
 }
