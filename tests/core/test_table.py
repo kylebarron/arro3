822118162fb87e7075fac99514fccbd63be06cd4
Kyle Barron <kylebarron2@gmail.com>
2024-08-01 01:06:31 -0400

    Misc data type fixes (#101)

--- tests/core/test_table.py
+++ tests/core/test_table.py
@@ -1,6 +1,8 @@
 import geoarrow.types as gt
 import numpy as np
+import pandas as pd
 import pyarrow as pa
+import pytest
 from arro3.core import ChunkedArray, Table
 
 
@@ -44,3 +46,12 @@ def test_table_append_array_extension_type():
     meta = geo_table.schema["geometry"].metadata
     assert b"ARROW:extension:name" in meta.keys()
     assert meta[b"ARROW:extension:name"] == b"geoarrow.point"
+
+
+@pytest.mark.xfail
+# from_batches fails on empty column with positive length
+def test_table_from_batches_empty_columns_with_len():
+    df = pd.DataFrame({"a": [1, 2, 3]})
+    no_columns = df[[]]
+    pa_table = pa.Table.from_pandas(no_columns)
+    _table = Table.from_batches(pa_table.to_batches())
