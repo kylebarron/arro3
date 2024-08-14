d4f6c77778eb7ae59eadb31630bb51e104ffc92f
Kyle Barron <kylebarron2@gmail.com>
2024-08-14 00:43:03 -0400

    Fix api docs

--- arro3-io/python/arro3/io/_io.pyi
+++ arro3-io/python/arro3/io/_io.pyi
@@ -1,12 +1,11 @@
 from pathlib import Path
 from typing import IO, Literal, Sequence
 
-from arro3.core import RecordBatchReader, Schema
-from arro3.core.types import (
-    ArrowSchemaExportable,
-    ArrowArrayExportable,
-    ArrowStreamExportable,
-)
+# Note: importing with
+# `from arro3.core import Array`
+# will cause Array to be included in the generated docs in this module.
+import arro3.core as core
+import arro3.core.types as types
 
 #### CSV
 
@@ -20,10 +19,29 @@ def infer_csv_schema(
     quote: str | None = None,
     terminator: str | None = None,
     comment: str | None = None,
-) -> Schema: ...
+) -> core.Schema:
+    """Infer a CSV file's schema
+
+    If `max_records` is `None`, all records will be read, otherwise up to `max_records`
+    records are read to infer the schema
+
+    Args:
+        file: The input CSV path or buffer.
+        has_header: Set whether the CSV file has a header. Defaults to None.
+        max_records: The maximum number of records to read to infer schema. Defaults to None.
+        delimiter: Set the CSV file's column delimiter as a byte character. Defaults to None.
+        escape: Set the CSV escape character. Defaults to None.
+        quote: Set the CSV quote character. Defaults to None.
+        terminator: Set the line terminator. Defaults to None.
+        comment: Set the comment character. Defaults to None.
+
+    Returns:
+        inferred schema from data
+    """
+
 def read_csv(
     file: IO[bytes] | Path | str,
-    schema: ArrowSchemaExportable,
+    schema: types.ArrowSchemaExportable,
     *,
     has_header: bool | None = None,
     batch_size: int | None = None,
@@ -32,9 +50,26 @@ def read_csv(
     quote: str | None = None,
     terminator: str | None = None,
     comment: str | None = None,
-) -> RecordBatchReader: ...
+) -> core.RecordBatchReader:
+    """Read a CSV file to an Arrow RecordBatchReader.
+
+    Args:
+        file: The input CSV path or buffer.
+        schema: The Arrow schema for this CSV file. Use [infer_csv_schema][arro3.io.infer_csv_schema] to infer an Arrow schema if needed.
+        has_header: Set whether the CSV file has a header. Defaults to None.
+        batch_size: Set the batch size (number of records to load at one time). Defaults to None.
+        delimiter: Set the CSV file's column delimiter as a byte character. Defaults to None.
+        escape: Set the CSV escape character. Defaults to None.
+        quote: Set the CSV quote character. Defaults to None.
+        terminator: Set the line terminator. Defaults to None.
+        comment: Set the comment character. Defaults to None.
+
+    Returns:
+        A RecordBatchReader with read CSV data
+    """
+
 def write_csv(
-    data: ArrowStreamExportable | ArrowArrayExportable,
+    data: types.ArrowStreamExportable | types.ArrowArrayExportable,
     file: IO[bytes] | Path | str,
     *,
     header: bool | None = None,
@@ -47,7 +82,29 @@ def write_csv(
     timestamp_format: str | None = None,
     timestamp_tz_format: str | None = None,
     null: str | None = None,
-) -> None: ...
+) -> None:
+    """Write an Arrow Table or stream to a CSV file.
+
+    Args:
+        data: The input Arrow table or stream.
+        file: The output buffer or file path for where to write the CSV.
+        header: Set whether to write the CSV file with a header. Defaults to None.
+        delimiter: Set the CSV file's column delimiter as a byte character. Defaults to None.
+        escape: Set the CSV file's escape character as a byte character.
+
+            In some variants of CSV, quotes are escaped using a special escape character
+            like `\\` (instead of escaping quotes by doubling them).
+
+            By default, writing these idiosyncratic escapes is disabled, and is only
+            used when double_quote is disabled. Defaults to None.
+        quote: Set the CSV file's quote character as a byte character. Defaults to None.
+        date_format: Set the CSV file's date format. Defaults to None.
+        datetime_format: Set the CSV file's datetime format. Defaults to None.
+        time_format: Set the CSV file's time format. Defaults to None.
+        timestamp_format: Set the CSV file's timestamp format. Defaults to None.
+        timestamp_tz_format: Set the CSV file's timestamp tz format. Defaults to None.
+        null: Set the value to represent null in output. Defaults to None.
+    """
 
 #### JSON
 
@@ -55,36 +112,112 @@ def infer_json_schema(
     file: IO[bytes] | Path | str,
     *,
     max_records: int | None = None,
-) -> Schema: ...
+) -> core.Schema:
+    """
+    Infer the schema of a JSON file by reading the first n records of the buffer, with
+    `max_records` controlling the maximum number of records to read.
+
+    Args:
+        file: The input JSON path or buffer.
+        max_records: The maximum number of records to read to infer schema. If not provided, will read the entire file to deduce field types. Defaults to None.
+
+    Returns:
+        Inferred Arrow Schema
+    """
+
 def read_json(
     file: IO[bytes] | Path | str,
-    schema: ArrowSchemaExportable,
+    schema: types.ArrowSchemaExportable,
     *,
     batch_size: int | None = None,
-) -> RecordBatchReader: ...
+) -> core.RecordBatchReader:
+    """Reads JSON data with a known schema into Arrow
+
+    Args:
+        file: The JSON file or buffer to read from.
+        schema: The Arrow schema representing the JSON data.
+        batch_size: Set the batch size (number of records to load at one time). Defaults to None.
+
+    Returns:
+        An arrow RecordBatchReader.
+    """
+
 def write_json(
-    data: ArrowStreamExportable | ArrowArrayExportable,
+    data: types.ArrowStreamExportable | types.ArrowArrayExportable,
     file: IO[bytes] | Path | str,
     *,
     explicit_nulls: bool | None = None,
-) -> None: ...
+) -> None:
+    """Write
+
+    By default the writer will skip writing keys with null values for backward
+    compatibility.
+
+    Args:
+        data: the Arrow Table, RecordBatchReader, or RecordBatch to write.
+        file: the output file or buffer to write to
+        explicit_nulls: Set whether to keep keys with null values, or to omit writing them. Defaults to skipping nulls.
+    """
+
 def write_ndjson(
-    data: ArrowStreamExportable | ArrowArrayExportable,
+    data: types.ArrowStreamExportable | types.ArrowArrayExportable,
     file: IO[bytes] | Path | str,
     *,
     explicit_nulls: bool | None = None,
-) -> None: ...
+) -> None:
+    """
+
+    By default the writer will skip writing keys with null values for backward compatibility.
+
+    Args:
+        data: the Arrow Table, RecordBatchReader, or RecordBatch to write.
+        file: the output file or buffer to write to
+        explicit_nulls: Set whether to keep keys with null values, or to omit writing them. Defaults to skipping nulls.
+    """
 
 #### IPC
 
-def read_ipc(file: IO[bytes] | Path | str) -> RecordBatchReader: ...
-def read_ipc_stream(file: IO[bytes] | Path | str) -> RecordBatchReader: ...
+def read_ipc(file: IO[bytes] | Path | str) -> core.RecordBatchReader:
+    """Read an Arrow IPC file into memory
+
+    Args:
+        file: The input Arrow IPC file path or buffer.
+
+    Returns:
+        An arrow RecordBatchReader.
+    """
+
+def read_ipc_stream(file: IO[bytes] | Path | str) -> core.RecordBatchReader:
+    """Read an Arrow IPC stream into memory
+
+    Args:
+        file: The input Arrow IPC stream path or buffer.
+
+    Returns:
+        An arrow RecordBatchReader.
+    """
+
 def write_ipc(
-    data: ArrowStreamExportable | ArrowArrayExportable, file: IO[bytes] | Path | str
-) -> None: ...
+    data: types.ArrowStreamExportable | types.ArrowArrayExportable,
+    file: IO[bytes] | Path | str,
+) -> None:
+    """Write Arrow data to an Arrow IPC file
+
+    Args:
+        data: the Arrow Table, RecordBatchReader, or RecordBatch to write.
+        file: the output file or buffer to write to
+    """
+
 def write_ipc_stream(
-    data: ArrowStreamExportable | ArrowArrayExportable, file: IO[bytes] | Path | str
-) -> None: ...
+    data: types.ArrowStreamExportable | types.ArrowArrayExportable,
+    file: IO[bytes] | Path | str,
+) -> None:
+    """Write Arrow data to an Arrow IPC stream
+
+    Args:
+        data: the Arrow Table, RecordBatchReader, or RecordBatch to write.
+        file: the output file or buffer to write to
+    """
 
 #### Parquet
 
@@ -110,18 +243,18 @@ ParquetEncoding = Literal[
 ]
 """Allowed Parquet encodings."""
 
-def read_parquet(file: Path | str) -> RecordBatchReader:
+def read_parquet(file: Path | str) -> core.RecordBatchReader:
     """Read a Parquet file to an Arrow RecordBatchReader
 
     Args:
-        file: _description_
+        file: The input Parquet file path or buffer.
 
     Returns:
-        _description_
+        The
     """
 
 def write_parquet(
-    data: ArrowStreamExportable | ArrowArrayExportable,
+    data: types.ArrowStreamExportable | types.ArrowArrayExportable,
     file: IO[bytes] | Path | str,
     *,
     bloom_filter_enabled: bool | None = None,
