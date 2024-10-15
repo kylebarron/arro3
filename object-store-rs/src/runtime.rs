c2216311162998c454348c7f6ecb805c8d0021c9
Kyle Barron <kyle@developmentseed.org>
2024-10-15 00:13:15 -0400

    put in arc

--- object-store-rs/src/runtime.rs
+++ object-store-rs/src/runtime.rs
@@ -1,3 +1,5 @@
+use std::sync::Arc;
+
 use pyo3::exceptions::PyValueError;
 use pyo3::prelude::*;
 use pyo3::sync::GILOnceCell;
@@ -5,15 +7,16 @@ use pyo3::types::PyBytes;
 use pyo3_object_store::PyObjectStore;
 use tokio::runtime::Runtime;
 
-static RUNTIME: GILOnceCell<Runtime> = GILOnceCell::new();
+static RUNTIME: GILOnceCell<Arc<Runtime>> = GILOnceCell::new();
 
 /// Get the tokio runtime for sync requests
-pub(crate) fn get_runtime(py: Python<'_>) -> PyResult<&Runtime> {
-    RUNTIME.get_or_try_init(py, || {
-        Runtime::new().map_err(|err| {
+pub(crate) fn get_runtime(py: Python<'_>) -> PyResult<Arc<Runtime>> {
+    let runtime = RUNTIME.get_or_try_init(py, || {
+        Ok::<_, PyErr>(Arc::new(Runtime::new().map_err(|err| {
             PyValueError::new_err(format!("Could not create tokio runtime. {}", err))
-        })
-    })
+        })?))
+    })?;
+    Ok(runtime.clone())
 }
 
 #[pyfunction]
