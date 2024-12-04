0b8bb44611bc6ee2dda76838ddb6d9393a4e08ea
Kyle Barron <kyle@developmentseed.org>
2024-12-04 11:57:55 -0500

    Bump arro3 to pyo3 0.23

--- arro3-io/src/utils.rs
+++ arro3-io/src/utils.rs
@@ -25,14 +25,13 @@ impl FileReader {
 
 impl<'py> FromPyObject<'py> for FileReader {
     fn extract_bound(ob: &Bound<'py, PyAny>) -> PyResult<Self> {
-        let py = ob.py();
         if let Ok(path) = ob.extract::<PathBuf>() {
             Ok(Self::File(File::open(path)?))
         } else if let Ok(path) = ob.extract::<String>() {
             Ok(Self::File(File::open(path)?))
         } else {
             Ok(Self::FileLike(PyFileLikeObject::with_requirements(
-                ob.into_py(py),
+                ob.clone().unbind(),
                 true,
                 false,
                 true,
@@ -115,14 +114,13 @@ pub enum FileWriter {
 
 impl<'py> FromPyObject<'py> for FileWriter {
     fn extract_bound(ob: &Bound<'py, PyAny>) -> PyResult<Self> {
-        let py = ob.py();
         if let Ok(path) = ob.extract::<PathBuf>() {
             Ok(Self::File(File::create(path)?))
         } else if let Ok(path) = ob.extract::<String>() {
             Ok(Self::File(File::create(path)?))
         } else {
             Ok(Self::FileLike(PyFileLikeObject::with_requirements(
-                ob.into_py(py),
+                ob.clone().unbind(),
                 false,
                 true,
                 true,
