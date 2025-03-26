d28b8e97afba31278c8e84b6f7680debeb5f7987
Kyle Barron <kyle@developmentseed.org>
2025-03-26 18:43:19 -0400

    Comment out PyObspecReader for now

--- arro3-io/src/source/async.rs
+++ arro3-io/src/source/async.rs
@@ -1,69 +1,73 @@
+use std::ops::Range;
 use std::sync::Arc;
 
 use bytes::Bytes;
 use futures::future::BoxFuture;
 use parquet::arrow::async_reader::ParquetObjectReader;
 use parquet::file::metadata::ParquetMetaData;
-use pyo3::prelude::*;
 
-struct PyObspecReader(PyObject);
+// TODO: come back to this to implement for arbitrary async Python backends
+// struct PyObspecReader(PyObject);
 
-impl Clone for PyObspecReader {
-    fn clone(&self) -> Self {
-        Self(Python::with_gil(|py| self.0.clone_ref(py)))
-    }
-}
+// impl Clone for PyObspecReader {
+//     fn clone(&self) -> Self {
+//         Self(Python::with_gil(|py| self.0.clone_ref(py)))
+//     }
+// }
 
-impl parquet::arrow::async_reader::AsyncFileReader for PyObspecReader {
-    fn get_bytes(
-        &mut self,
-        range: std::ops::Range<usize>,
-    ) -> BoxFuture<'_, parquet::errors::Result<Bytes>> {
-        todo!()
-    }
+// impl parquet::arrow::async_reader::AsyncFileReader for PyObspecReader {
+//     fn get_bytes(&mut self, range: Range<usize>) -> BoxFuture<'_, parquet::errors::Result<Bytes>> {
+//         todo!()
+//     }
 
-    fn get_byte_ranges(
-        &mut self,
-        ranges: Vec<std::ops::Range<usize>>,
-    ) -> BoxFuture<'_, parquet::errors::Result<Vec<Bytes>>> {
-        todo!()
-    }
+//     fn get_byte_ranges(
+//         &mut self,
+//         ranges: Vec<Range<usize>>,
+//     ) -> BoxFuture<'_, parquet::errors::Result<Vec<Bytes>>> {
+//         todo!()
+//     }
 
-    fn get_metadata(&mut self) -> BoxFuture<'_, parquet::errors::Result<Arc<ParquetMetaData>>> {
-        todo!()
-    }
-}
+//     fn get_metadata(&mut self) -> BoxFuture<'_, parquet::errors::Result<Arc<ParquetMetaData>>> {
+//         Box::pin(async move {
+//             let file_size = self.meta.size;
+//             let metadata = ParquetMetaDataReader::new()
+//                 .with_column_indexes(self.preload_column_index)
+//                 .with_offset_indexes(self.preload_offset_index)
+//                 .with_prefetch_hint(self.metadata_size_hint)
+//                 .load_and_finish(self, file_size)
+//                 .await?;
+//             Ok(Arc::new(metadata))
+//         })
+//     }
+// }
 
 #[derive(Clone)]
 pub(crate) enum AsyncReader {
-    Python(PyObspecReader),
+    // Python(PyObspecReader),
     ObjectStore(ParquetObjectReader),
 }
 
 impl parquet::arrow::async_reader::AsyncFileReader for AsyncReader {
-    fn get_bytes(
-        &mut self,
-        range: std::ops::Range<usize>,
-    ) -> BoxFuture<'_, parquet::errors::Result<Bytes>> {
+    fn get_bytes(&mut self, range: Range<usize>) -> BoxFuture<'_, parquet::errors::Result<Bytes>> {
         match self {
-            Self::Python(reader) => reader.get_bytes(range),
+            // Self::Python(reader) => reader.get_bytes(range),
             Self::ObjectStore(reader) => reader.get_bytes(range),
         }
     }
 
     fn get_byte_ranges(
         &mut self,
-        ranges: Vec<std::ops::Range<usize>>,
+        ranges: Vec<Range<usize>>,
     ) -> BoxFuture<'_, parquet::errors::Result<Vec<Bytes>>> {
         match self {
-            Self::Python(reader) => reader.get_byte_ranges(ranges),
+            // Self::Python(reader) => reader.get_byte_ranges(ranges),
             Self::ObjectStore(reader) => reader.get_byte_ranges(ranges),
         }
     }
 
     fn get_metadata(&mut self) -> BoxFuture<'_, parquet::errors::Result<Arc<ParquetMetaData>>> {
         match self {
-            Self::Python(reader) => reader.get_metadata(),
+            // Self::Python(reader) => reader.get_metadata(),
             Self::ObjectStore(reader) => reader.get_metadata(),
         }
     }
