93b3ff5e571edcc8f397d08a01c312ff9807ccdd
Kyle Barron <kyle@developmentseed.org>
2025-08-21 18:15:18 -0400

    feat(arro3): Import datetime64 arrays from numpy

--- tests/core/test_numpy.py
+++ tests/core/test_numpy.py
@@ -1,4 +1,7 @@
+from datetime import date, datetime
+
 import numpy as np
+import pyarrow as pa
 from arro3.core import Array, DataType
 
 
@@ -39,3 +42,124 @@ def test_string_to_numpy():
 
     arr = Array(string_list, DataType.string_view())
     assert np.array_equal(arr.to_numpy(), expected)
+
+
+def test_date_from_numpy():
+    dates = [date(2023, 1, 1), date(2023, 1, 2)]
+    data = np.array(dates, dtype="datetime64[D]")
+    arr = Array.from_numpy(data)
+    assert arr.type == DataType.date64()
+    assert arr[0].as_py() == dates[0]
+    assert arr[1].as_py() == dates[1]
+
+
+def test_date_from_numpy_non_contiguous():
+    dates = [
+        date(2023, 1, 1),
+        date(2023, 1, 2),
+        date(2023, 1, 3),
+    ]
+    data = np.array(dates, dtype="datetime64[D]")[::2]
+    arr = Array.from_numpy(data)
+    assert arr.type == DataType.date64()
+    assert arr[0].as_py() == dates[0]
+    assert arr[1].as_py() == dates[2]
+
+
+def test_seconds_from_numpy():
+    seconds = [datetime(2023, 1, 1, 0, 0, 0), datetime(2023, 1, 2, 0, 0, 0)]
+    data = np.array(seconds, dtype="datetime64[s]")
+    arr = Array.from_numpy(data)
+    assert arr.type == DataType.timestamp("s")
+    assert arr[0].as_py() == seconds[0]
+    assert arr[1].as_py() == seconds[1]
+
+
+def test_seconds_from_numpy_non_contiguous():
+    seconds = [
+        datetime(2023, 1, 1, 0, 0, 0),
+        datetime(2023, 1, 2, 0, 0, 0),
+        datetime(2023, 1, 3, 0, 0, 0),
+    ]
+    data = np.array(seconds, dtype="datetime64[s]")[::2]
+    arr = Array.from_numpy(data)
+    assert arr.type == DataType.timestamp("s")
+    assert arr[0].as_py() == seconds[0]
+    assert arr[1].as_py() == seconds[2]
+
+
+def test_milliseconds_from_numpy():
+    milliseconds = [
+        datetime(2023, 1, 1, 0, 0, 0, 123000),
+        datetime(2023, 1, 2, 0, 0, 0, 654000),
+    ]
+    data = np.array(milliseconds, dtype="datetime64[ms]")
+    arr = Array.from_numpy(data)
+    assert arr.type == DataType.timestamp("ms")
+    assert arr[0].as_py() == milliseconds[0]
+    assert arr[1].as_py() == milliseconds[1]
+
+
+def test_milliseconds_from_numpy_non_contiguous():
+    milliseconds = [
+        datetime(2023, 1, 1, 0, 0, 0, 123000),
+        datetime(2023, 1, 2, 0, 0, 0, 654000),
+        datetime(2023, 1, 3, 0, 0, 0, 789000),
+    ]
+    data = np.array(milliseconds, dtype="datetime64[ms]")[::2]
+    arr = Array.from_numpy(data)
+    assert arr.type == DataType.timestamp("ms")
+    assert arr[0].as_py() == milliseconds[0]
+    assert arr[1].as_py() == milliseconds[2]
+
+
+def test_microseconds_from_numpy():
+    microseconds = [
+        datetime(2023, 1, 1, 0, 0, 0, 123456),
+        datetime(2023, 1, 2, 0, 0, 0, 654321),
+    ]
+    data = np.array(microseconds, dtype="datetime64[us]")
+    arr = Array.from_numpy(data)
+    assert arr.type == DataType.timestamp("us")
+    assert arr[0].as_py() == microseconds[0]
+    assert arr[1].as_py() == microseconds[1]
+
+
+def test_microseconds_from_numpy_non_contiguous():
+    microseconds = [
+        datetime(2023, 1, 1, 0, 0, 0, 123456),
+        datetime(2023, 1, 2, 0, 0, 0, 654321),
+        datetime(2023, 1, 3, 0, 0, 0, 789012),
+    ]
+    data = np.array(microseconds, dtype="datetime64[us]")[::2]
+    arr = Array.from_numpy(data)
+    assert arr.type == DataType.timestamp("us")
+    assert arr[0].as_py() == microseconds[0]
+    assert arr[1].as_py() == microseconds[2]
+
+
+def test_nanoseconds_from_numpy():
+    date_strings = [
+        "2023-01-01T10:30:00.123456789",
+        "2023-01-01T10:30:00.987654321",
+    ]
+    data = np.array(date_strings, dtype="datetime64[ns]")
+    arr = Array.from_numpy(data)
+    assert arr.type == DataType.timestamp("ns")
+
+    # We can't go through Python datetime because it doesn't support nanoseconds
+    assert pa.array(arr) == pa.array(data)
+
+
+def test_nanoseconds_from_numpy_non_contiguous():
+    date_strings = [
+        "2023-01-01T10:30:00.123456789",
+        "2023-01-01T10:30:00.987654321",
+        "2023-01-01T10:30:00.111111111",
+    ]
+    data = np.array(date_strings, dtype="datetime64[ns]")[::2]
+    arr = Array.from_numpy(data)
+    assert arr.type == DataType.timestamp("ns")
+
+    # We can't go through Python datetime because it doesn't support nanoseconds
+    assert pa.array(arr) == pa.array(data)
