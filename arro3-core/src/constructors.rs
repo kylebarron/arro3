112d2b9024406156ce323c16d5e750d01f0473af
Kyle Barron <kyle@developmentseed.org>
2025-08-21 20:36:03 -0400

    feat(arro3-core): add mask parameter to list/struct constructors

--- arro3-core/src/constructors.rs
+++ arro3-core/src/constructors.rs
@@ -3,7 +3,7 @@ use std::sync::Arc;
 use arrow_array::cast::AsArray;
 use arrow_array::types::{Int32Type, Int64Type};
 use arrow_array::{Array, ArrayRef, FixedSizeListArray, LargeListArray, ListArray, StructArray};
-use arrow_buffer::OffsetBuffer;
+use arrow_buffer::{NullBuffer, NullBufferBuilder, OffsetBuffer};
 use arrow_schema::{DataType, Field};
 use pyo3::exceptions::PyValueError;
 use pyo3::prelude::*;
@@ -13,11 +13,12 @@ use pyo3_arrow::export::Arro3Array;
 use pyo3_arrow::{PyArray, PyField};
 
 #[pyfunction]
-#[pyo3(signature=(values, list_size, *, r#type=None))]
+#[pyo3(signature=(values, list_size, *, r#type=None, mask=None))]
 pub(crate) fn fixed_size_list_array(
     values: PyArray,
     list_size: i32,
     r#type: Option<PyField>,
+    mask: Option<PyArray>,
 ) -> PyArrowResult<Arro3Array> {
     let (values_array, values_field) = values.into_inner();
     let output_field = r#type.map(|t| t.into_inner()).unwrap_or_else(|| {
@@ -35,16 +36,18 @@ pub(crate) fn fixed_size_list_array(
             )
         }
     };
-    let array = FixedSizeListArray::try_new(inner_field.clone(), list_size, values_array, None)?;
+    let nulls = create_nulls(mask)?;
+    let array = FixedSizeListArray::try_new(inner_field.clone(), list_size, values_array, nulls)?;
     Ok(PyArray::new(Arc::new(array), output_field).into())
 }
 
 #[pyfunction]
-#[pyo3(signature=(offsets, values, *, r#type=None))]
+#[pyo3(signature=(offsets, values, *, r#type=None, mask=None))]
 pub(crate) fn list_array(
     offsets: PyArray,
     values: PyArray,
     r#type: Option<PyField>,
+    mask: Option<PyArray>,
 ) -> PyArrowResult<Arro3Array> {
     let (values_array, values_field) = values.into_inner();
     let (offsets_array, _) = offsets.into_inner();
@@ -77,30 +80,32 @@ pub(crate) fn list_array(
         }
     };
 
+    let nulls = create_nulls(mask)?;
     let list_array: ArrayRef = if large_offsets {
         Arc::new(LargeListArray::try_new(
             inner_field.clone(),
             OffsetBuffer::new(offsets_array.as_primitive::<Int64Type>().values().clone()),
             values_array,
-            None,
+            nulls,
         )?)
     } else {
         Arc::new(ListArray::try_new(
             inner_field.clone(),
             OffsetBuffer::new(offsets_array.as_primitive::<Int32Type>().values().clone()),
             values_array,
-            None,
+            nulls,
         )?)
     };
     Ok(PyArray::new(Arc::new(list_array), output_field).into())
 }
 
 #[pyfunction]
-#[pyo3(signature=(arrays, *, fields, r#type=None))]
+#[pyo3(signature=(arrays, *, fields, r#type=None, mask=None))]
 pub(crate) fn struct_array(
     arrays: Vec<PyArray>,
     fields: Vec<PyField>,
     r#type: Option<PyField>,
+    mask: Option<PyArray>,
 ) -> PyArrowResult<Arro3Array> {
     let output_field = r#type.map(|t| t.into_inner()).unwrap_or_else(|| {
         let fields = fields
@@ -122,6 +127,28 @@ pub(crate) fn struct_array(
         })
         .collect::<Vec<_>>();
 
-    let array = StructArray::try_new(inner_fields, arrays, None)?;
+    let nulls = create_nulls(mask)?;
+    let array = StructArray::try_new(inner_fields, arrays, nulls)?;
     Ok(PyArray::new(Arc::new(array), output_field).into())
+}
+
+fn create_nulls(mask: Option<PyArray>) -> PyArrowResult<Option<NullBuffer>> {
+    if let Some(mask) = mask {
+        let (mask_array, _) = mask.into_inner();
+        let bool_arr = mask_array
+            .as_boolean_opt()
+            .ok_or_else(|| PyValueError::new_err("Expected mask to be boolean array"))?;
+        let mut builder = NullBufferBuilder::new(bool_arr.len());
+        for val in bool_arr.values().iter() {
+            // In pyarrow semantics, a `True` value in the mask means the value is null
+            if val {
+                builder.append_null();
+            } else {
+                builder.append_non_null();
+            }
+        }
+        Ok(builder.finish())
+    } else {
+        Ok(None)
+    }
 }
--- tests/core/test_constructors.py
+++ tests/core/test_constructors.py
@@ -31,6 +31,29 @@ def test_fixed_size_list_array_with_type():
     assert pa_array.type.field(0).name == "inner"
 
 
+def test_fixed_size_list_array_with_mask():
+    np_arr = np.array([1, 2, 3, 4, 5, 6], dtype=np.float64)
+    flat_array = Array.from_numpy(np_arr)
+
+    np_mask = np.array([True, False, True], dtype=bool)
+    mask = Array.from_numpy(np_mask)
+
+    arro3_array = fixed_size_list_array(flat_array, 2, mask=mask)
+
+    # Note that we don't exactly match the pyarrow array because we still allocate for
+    # null values.
+    pa_arr = pa.array(
+        [[1, 2], [3, 4], [5, 6]],
+        type=pa.field(arro3_array.type).type,
+        mask=np_mask,
+    )
+
+    assert arro3_array[0].is_valid == pa_arr[0].is_valid
+    assert arro3_array[1].is_valid == pa_arr[1].is_valid
+    assert arro3_array[1] == Array(pa_arr)[1]
+    assert arro3_array[2].is_valid == pa_arr[2].is_valid
+
+
 def test_list_array():
     np_arr = np.array([1, 2, 3, 4, 5, 6], dtype=np.float64)
     flat_array = Array.from_numpy(np_arr)
@@ -54,6 +77,28 @@ def test_list_array_with_type():
     assert pa_array.type.field(0).name == "inner"
 
 
+def test_list_array_with_mask():
+    np_arr = np.array([1, 2, 3, 4, 5, 6], dtype=np.float64)
+    flat_array = Array.from_numpy(np_arr)
+    offsets_array = Array.from_numpy(np.array([0, 2, 5, 6], dtype=np.int32))
+
+    np_mask = np.array([True, False, True], dtype=bool)
+    mask = Array.from_numpy(np_mask)
+
+    arro3_array = list_array(offsets_array, flat_array, mask=mask)
+
+    # Note that we don't exactly match the pyarrow array because we still allocate for
+    # null values.
+    pa_arr = pa.array(
+        [[1, 2], [3, 4, 5], [6]], type=pa.field(arro3_array.type).type, mask=np_mask
+    )
+
+    assert arro3_array[0].is_valid == pa_arr[0].is_valid
+    assert arro3_array[1].is_valid == pa_arr[1].is_valid
+    assert arro3_array[1] == Array(pa_arr)[1]
+    assert arro3_array[2].is_valid == pa_arr[2].is_valid
+
+
 def test_struct_array():
     a = pa.array([1, 2, 3, 4])
     b = pa.array(["a", "b", "c", "d"])
@@ -63,3 +108,32 @@ def test_struct_array():
     assert pa.types.is_struct(pa_type)
     assert pa_type.field(0).name == "a"
     assert pa_type.field(1).name == "b"
+
+
+def test_struct_array_with_mask():
+    a = pa.array([1, 2, 3, 4])
+    b = pa.array(["a", "b", "c", "d"])
+
+    np_mask = np.array([True, False, True, False], dtype=bool)
+    mask = Array.from_numpy(np_mask)
+
+    arro3_arr = struct_array(
+        [a, b],
+        fields=[Field("a", a.type), Field("b", b.type)],
+        mask=mask,
+    )
+
+    pa_arr = pa.array(
+        [
+            {"a": 1, "b": "a"},
+            {"a": 2, "b": "b"},
+            {"a": 3, "b": "c"},
+            {"a": 4, "b": "d"},
+        ],
+        type=pa.field(arro3_arr.type).type,
+        mask=np_mask,
+    )
+
+    for i in range(len(arro3_arr)):
+        assert arro3_arr[i].is_valid == pa_arr[i].is_valid
+        assert arro3_arr[i] == Array(pa_arr)[i]
