45f86484205ae7c9f991ac3ca7db1dc778aa846a
Ivan <surister98@gmail.com>
2025-11-04 16:42:18 +0100

    Update pyo3-arrow/src/table.rs
    Co-authored-by: Kyle Barron <kylebarron2@gmail.com>

--- pyo3-arrow/src/table.rs
+++ pyo3-arrow/src/table.rs
@@ -562,7 +562,7 @@ impl PyTable {
 
     fn remove_column(&self, i: usize) -> PyArrowResult<Arro3Table> {
         if i > self.num_columns() {
-            return Err(PyIndexError::new_err(format!("Invalid column index \"{}\"", i)).into());
+            return Err(PyIndexError::new_err(format!("Invalid column index \"{i}\"")).into());
         }
 
         let mut fields = self.schema.fields().to_vec();
